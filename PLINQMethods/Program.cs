b726883c012da63a374e6843f09dffaeabf80e0d
muratacarr <acarrmuratt@gmail.com>
2023-10-28 18:37:11 +0300

    PLINQ tamamlandı

---
+++ PLINQMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+Stopwatch sw = Stopwatch.StartNew();
+
+//metohd içlerinde console yazdırmadığımız zaman 1milyodan sonra Parallel daha avantajlı olmaya başladı
+var array=Enumerable.Range(1,1000000000).ToList();
+
+var newArray=array.AsParallel().Where(x=>x%2==0);
+
+sw.Start();
+
+newArray.ForAll(x =>
+{
+    //Console.WriteLine(x + "   Kullanılan Thread : " + Thread.CurrentThread.ManagedThreadId);
+});
+
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+sw.Restart();
+
+sw.Start();
+
+newArray.ToList().ForEach(x =>
+{
+    //Console.WriteLine(x + "   Kullanılan Thread : " + Thread.CurrentThread.ManagedThreadId);
+
+});
+
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
--- TaskParallelLibrary/Program.cs
+++ TaskParallelLibrary/Program.cs
@@ -96,8 +96,7 @@ Islem bitti : 4687
          * 
          */
 
-
-
+        //race condition
         int deger = 0;
 
         Parallel.ForEach(Enumerable.Range(1, 100000).ToList(), x =>
